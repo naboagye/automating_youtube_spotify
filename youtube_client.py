941547c2b4dc79682cf20d354971c66486520bb9
Omar <myles.brath@hotmail.com>
2020-10-09 21:49:53 +0100

    finished youtube_cl

--- youtube_client.py
+++ youtube_client.py
@@ -1,22 +1,25 @@
 import os
 
-import google_auth_oauthlib
-import googleapiclient
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 import youtube_dl
 
-class Playlist():
-    def __init__(self, id , title):
+
+class Playlist(object):
+    def __init__(self, id, title):
         self.id = id
         self.title = title
 
-class Song():
+
+class Song(object):
     def __init__(self, artist, track):
         self.artist = artist
         self.track = track
 
-class YouTubeClient():
+
+class YouTubeClient(object):
     # grants access to users YouTube account
-    def __init__(self, crendentials_location):
+    def __init__(self, credentials_location):
         # Copied from Youtube Data v3 Sample code
         scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
@@ -29,18 +32,19 @@ class YouTubeClient():
 
         # Get credentials and create an API client
         flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-            crendentials_location, scopes)
+            credentials_location, scopes)
         credentials = flow.run_console()
         youtube_client = googleapiclient.discovery.build(
             api_service_name, api_version, credentials=credentials)
 
         self.youtube_client = youtube_client
 
+
 # Gets a list of the users playlist
 def get_playlist(self):
-    request = youtube.playlists().list(
+    request = self.youtube_client.playlists().list(
         part="id,snippet",
-        maxResults=50
+        maxResults=50,
         mine=True
     )
     response = request.execute()
@@ -48,6 +52,7 @@ def get_playlist(self):
     playlist = [Playlist(item['id'], item['snippet']['title']) for item in response['items']]
 
     return playlist
+
 
 # gets a list of the videos inside of the selected playlist
 def get_videos_from_playlist(self, playlist_id):
@@ -68,11 +73,10 @@ def get_videos_from_playlist(self, playlist_id):
     return songs
 
 
-
 def get_song_info_from_video(self, video_id):
     youtube_url = f'https://www.youtube.com/watch?v={video_id}'
 
-    video = youtube_dl.YoutubeDL({'quiet':True}).extract_info(
+    video = youtube_dl.YoutubeDL({'quiet': True}).extract_info(
         youtube_url, download=False
     )
 
@@ -80,5 +84,3 @@ def get_song_info_from_video(self, video_id):
     track = video['track']
 
     return artist, track
-
-
